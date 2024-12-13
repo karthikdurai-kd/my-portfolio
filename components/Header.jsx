dbab227726c6c841bfb05a8021cbc22cbb1038b4
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-12-13 18:35:04 -0500

    Added Header and Navbar Component

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Button } from "../components/ui/button";
+
+// Importing components
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Karthik<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* Desktop Nav and Hire Me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">Mobile Nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
