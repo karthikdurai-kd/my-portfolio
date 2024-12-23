b39b6a1bdc7808cafa603e74473ef1d91270fd50
Karthikdurai1994 <duraikarthik6@gmail.com>
2024-12-23 00:40:50 -0500

    create base template for project page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,4 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+// Projects Data
+const projects = [
+  {
+    num: "01",
+    category: "full stack",
+  },
+];
 
 const Projects = () => {
   return <div>Project Page</div>;
