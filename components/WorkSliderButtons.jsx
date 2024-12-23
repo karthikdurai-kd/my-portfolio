86b002fd77ad802582982f4b2d028a8b93d8ced0
Karthikdurai1994 <duraikarthik6@gmail.com>
2024-12-23 13:40:53 -0500

    Create Project Page UI and added project description contents

---
+++ components/WorkSliderButtons.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderButtons = () => {
+  return <div>WorkSliderButtons</div>;
+};
+
+export default WorkSliderButtons;
